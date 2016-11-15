be4ee9a1e9e19dea986667c0bbe8d8cfb03470b2
neal-o-r <neal@n-o-r.xyz>
2016-11-15 18:11:17 +0000

    plotting routine

--- ref.py
+++ ref.py
@@ -1,12 +1,14 @@
 import tensorflow as tf
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns; sns.set()
 import net as net
 import adv as adv
 import gen as gen
 
-fx = lambda x: 2*x**2 + x + 10
+fx = lambda x: x**2
 x_range = (-5,5)
-n_train = 4000
+n_train = 10000
 white_box = None
 
 def black_box(x_range, white_box=None):
@@ -17,37 +19,72 @@ def black_box(x_range, white_box=None):
 
         return (switch, one_hot)
 
+def make_chart(fx, pts, labels, guess):
 
-x = tf.placeholder(tf.float32, [1,1], name='x') 
+	flat_pts = np.ravel(pts).reshape(100,2)
+	sym_inds = np.ravel(np.argmax(labels, axis=2))
+	col_inds = np.ravel(guess)
+
+	syms = ['s','o']
+	cols = ['r', 'k']
+
+	for i, p in enumerate(flat_pts):
+		plt.scatter(p[0], p[1], marker=syms[sym_inds[i]], color=cols[col_inds[i]])
+
+	plt.show()
+
+
+
+X_gen = tf.placeholder(tf.float32, [1,1], name="X_gen") 
 
 X_adv = tf.placeholder(tf.float32, [1, 2], name="X_adv")
 Y_adv = tf.placeholder(tf.float32, [1, 2], name="Y_adv")
 
-gen_x = gen.generator(x)
+gen_x = gen.generator(X_gen)
 y_c = adv.adversary(X_adv)
 
-cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(y_c, Y_adv)) \
-       + tf.nn.l2_loss(1.0 - y_c[0][1])
+cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(y_c, Y_adv)) #\
+#       + tf.nn.l2_loss(1.0 - y_c[0][1])
 
 train_op = tf.train.AdamOptimizer().minimize(cost)
 test = tf.equal(tf.argmax(y_c, 1), tf.argmax(Y_adv, 1))
-
 
 with tf.Session() as sess:
         
         sess.run(tf.initialize_all_variables())
         
         for i in range(n_train):
-        	switch, label = black_box(x_range, white_box=1)
+        	switch, label = black_box(x_range)
 
         	if switch == 1:
-                	x_rand = [[np.random.uniform(x_range[0], x_range[1])]]
-			y_gen = sess.run(gen_x, feed_dict={x:x_rand})
-			pt = np.array([[x_rand[0][0], y_gen[0][0]]])
+                	x_rand = np.array([[np.random.uniform(x_range[0], x_range[1])]])
+			y_gen = fx(x_rand)#sess.run(gen_x, feed_dict={X_gen:x_rand})
+			pt = np.array([[x_rand[0][0], y_gen]])
 
 		else:
                 	x = np.random.uniform(x_range[0], x_range[1])
-                	y = np.random.uniform(x_range[0], x_range[1])
+                	y = np.random.uniform(0, 25)
         		pt = np.array([[np.float32(x), np.float32(y)]])
  
-                	sess.run(train_op, feed_dict={X_adv:pt, Y_adv:label})
+                sess.run(train_op, feed_dict={X_adv:pt, Y_adv:label})
+
+	pts, guess, ans = [], [], []
+	for i in range(100):
+
+        	switch, label = black_box(x_range)
+
+        	if switch == 1:
+                	x_rand = np.array([[np.random.uniform(x_range[0], x_range[1])]])
+			y_gen = fx(x_rand)#sess.run(gen_x, feed_dict={X_gen:x_rand})
+			pt = np.array([[x_rand[0][0], y_gen]])
+
+		else:
+                	x = np.random.uniform(x_range[0], x_range[1])
+                	y = np.random.uniform(0, 25)
+        		pt = np.array([[np.float32(x), np.float32(y)]])
+		pts.append(pt)
+		ans.append(label)
+
+                guess.append(sess.run(test, feed_dict={X_adv:pt, Y_adv:label}))
+
+	make_chart(fx, pts, ans, guess)
